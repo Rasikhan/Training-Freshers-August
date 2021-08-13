d321135bd56a8510e11acb479602d4c2aa121665
Rasikhan <rasikhan@provility.com>
2021-08-13 19:20:07 +0530

    Day1 forloop

--- day1.js
+++ day1.js
@@ -1,10 +1,23 @@
 //exe 1
 for (let i=0; i<=20; i++) {
     if(i%2==1) 
-      console.log([i]);
+      console.log(i);
     }
+    
+
     
     //exe 2
     for (let i=20; i>=1; i--) {
         console.log(i)
     }
+    //exe 3
+    
+    function test() {
+  
+        for(let i=1; i<=10; i++) {
+          console.log(9 * i);
+        
+        }
+        }
+        test();
+    
